31460aca828e8983e292e4a23cf096a9a72abffa
M3GH4N <115942157+M3GH4N@users.noreply.github.com>
2022-12-08 17:58:31 +0100

    Add files via upload

---
+++ WindowstyleHidden.ps1
@@ -0,0 +1,13 @@
+﻿<#
+
+Auteur : Méghan Rigoli
+Date : 04/09/2022
+Version : 1.0
+Description : Ce script permet de cacher et executer un autre script
+
+Source : https://www.delftstack.com/howto/powershell/powershell.exe-hidden/
+
+#>
+
+# On cache la fenêtre du script pour les utilisateurs
+PowerShell.exe -windowstyle hidden {\\SRV-AD.axeplane.loc\Scripts\SAUVEGARDE_PLANIFIEE.ps1}
